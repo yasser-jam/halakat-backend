78bdcbb3ef0f27dadb170ca3cda95dd446246266
Yasser-Jamal-Al-Deen <yasserjamalaldeen@gmail.com>
2025-06-22 19:47:44 +0300

    allow origin issue

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: '*',
+    origin: ['http://localhost:3000', 'https://ali-aldikker.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Accept, Authorization',
     credentials: true,
